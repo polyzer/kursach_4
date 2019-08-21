224eeb298c8f1fc96c57347633e16d1558d6fa6a
Игорь Токарев <realpolyzer@yandex.ru>
2019-08-21 21:40:45 +0000

    'update'

--- errors.py
+++ errors.py
@@ -1,11 +1,7 @@
 import re
-open_file = open('out.txt')
-#People id's
-errors_obj = {
-    "bs000": {
-    }
-}
-classes_names = []
+open_file = open('second.txt')
+fringe = 0
+classes_objects = []
 for i in range(0, 104):
     start_name = "bs"
     if i < 10:
@@ -13,10 +9,22 @@ for i in range(0, 104):
     elif i < 100:
         start_name += "0"
     start_name += str(i)
-    classes_names.append(start_name)
-print(classes_names)
+    new_el = {
+        "class_name": start_name,
+        "class_number": str(i),
+        "right_matches": 0,
+        "missed_matches": 0
+    }
+    classes_objects.append(new_el)
 
-# for line in open_file.readlines():
-#     print(line)
-#     result = re.split(r'i', 'Analytics Vidhya')
+for line in open_file.readlines():
+    for error_obj in classes_objects:
+        if line.startswith(error_obj['class_name']):
+            line_arr = line.split(' ')
+            if line_arr[6] == error_obj['class_number']:
+                error_obj['right_matches'] += 1
+            else:
+                error_obj['missed_matches'] += 1
+#    result = re.split(r'[[', 'Analytics Vidhya')
 open_file.close()
+print(classes_objects)
